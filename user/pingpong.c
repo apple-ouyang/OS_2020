8dc82e6e8787a82fe456c15c316f24980a201b9d
OuYang <1390334459@qq.com>
2020-10-27 21:55:36 +0800

    util前2个代码

---
+++ user/pingpong.c
@@ -0,0 +1,24 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argv, int **argc){
+	int pp[2], cp[2];
+	pipe(pp);
+	pipe(cp);
+
+	if(fork()==0){
+		char buf[10];
+		read(pp[0], buf, 5);
+		printf("%d: received %s",getpid(), buf);
+		write(cp[1], "pong\n", 5);
+		exit();
+	}else{
+		char buf[10];
+		write(pp[1], "ping\n", 5);
+		wait();
+		read(cp[0], buf, 5);
+		printf("%d: received %s",getpid(), buf);
+	}	
+	exit();
+}
---
+++ user/sleep.c
@@ -0,0 +1,13 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int argc, char** argv){
+	if(argc == 1){
+		printf("please input time!\n");
+		exit();
+	}
+	int t = atoi(argv[1]);
+	sleep(t);
+	exit();
+}
