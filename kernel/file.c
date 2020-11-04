a9aa014313321e72401b83a362c153ab6ad7e34b
ouyangsherlock <45086632+ouyangsherlock@users.noreply.github.com>
2020-11-04 09:59:06 +0800

    Revert "Alloc"

--- kernel/file.c
+++ kernel/file.c
@@ -16,7 +16,7 @@
 struct devsw devsw[NDEV];
 struct {
   struct spinlock lock;
-  // struct file file[NFILE];
+  struct file file[NFILE];
 } ftable;
 
 void
@@ -32,21 +32,15 @@ filealloc(void)
   struct file *f;
 
   acquire(&ftable.lock);
-  f = bd_malloc(sizeof(struct file));
-  if(f){
-    memset(f, 0, sizeof(struct file));
-    f->ref = 1;
+  for(f = ftable.file; f < ftable.file + NFILE; f++){
+    if(f->ref == 0){
+      f->ref = 1;
+      release(&ftable.lock);
+      return f;
+    }
   }
   release(&ftable.lock);
-  return f;
-  // for(f = ftable.file; f < ftable.file + NFILE; f++){
-  //   if(f->ref == 0){
-  //     f->ref = 1;
-  //     release(&ftable.lock);
-  //     return f;
-  //   }
-  // }
-  
+  return 0;
 }
 
 // Increment ref count for file f.
@@ -77,7 +71,6 @@ fileclose(struct file *f)
   ff = *f;
   f->ref = 0;
   f->type = FD_NONE;
-  bd_free(f);
   release(&ftable.lock);
 
   if(ff.type == FD_PIPE){
